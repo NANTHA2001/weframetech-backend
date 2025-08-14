376e37de4eb430f8c9d95a9a173ca4a69b4eeafc
NANTHA2001 <Nanthakumar.G@bigthinkcode.com>
2025-08-14 09:11:35 +0530

    updated code

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -7,7 +7,7 @@ const Users: CollectionConfig = {
   fields: [
     { name: 'name', type: 'text', required: true },
     { name: 'email', type: 'email', required: true }, 
-    { name: 'password', type: 'text', required: true },
+    // { name: 'password', type: 'text', required: true },
     { name: 'role', type: 'select', required: true, options: ['attendee','organizer','admin'], defaultValue: 'attendee' },
     { name: 'tenant', type: 'relationship', relationTo: 'tenants', required: true, admin: { position: 'sidebar' } },
   ],
